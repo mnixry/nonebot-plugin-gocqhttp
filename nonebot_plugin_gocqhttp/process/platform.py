9713081dd6cb126bb36bd59e693e7e169e2976f4
Mix <mnixry@users.noreply.github.com>
2022-02-12 15:06:35 +0800

    :recycle: Refactor get platform info method with `py-cpuinfo`

--- nonebot_plugin_gocqhttp/process/platform.py
+++ nonebot_plugin_gocqhttp/process/platform.py
@@ -1,28 +1,7 @@
 import platform
 
-_ARCHITECTURE_MAP = {
-    # For Windows, it uses PROCESSOR_ARCHITECTURE environment variable. Reference:
-    # https://docs.microsoft.com/en-us/windows/win32/winprog64/wow64-implementation-details#environment-variables
-    # https://github.com/python/cpython/blob/main/Lib/platform.py#L717-L727
-    "Windows": {
-        "AMD64": "amd64",
-        "X86": "386",
-        "ARM64": "arm64",
-    },
-    # For Unix-like system, it follows os.uname result. Reference:
-    # https://en.wikipedia.org/wiki/Uname
-    "Linux": {
-        "x86_64": "amd64",
-        "i686": "386",
-        "i386": "386",
-        "aarch64": "arm64",
-        "armv7l": "armv7",
-    },
-    "Darwin": {
-        "x86_64": "amd64",
-        "arm64": "arm64",
-    },
-}
+import psutil
+from cpuinfo import get_cpu_info
 
 _SYSTEM_MAP = {
     "Windows": "windows",
@@ -30,20 +9,24 @@ _SYSTEM_MAP = {
     "Darwin": "darwin",
 }
 
+_ARCHITECTURE_MAP = {
+    "X86_32": "386",
+    "X86_64": "amd64",
+    "ARM_7": "armv7",
+    "ARM_8": "arm64",
+}
+
 
 def _get_platform():
-    uname = platform.uname()
     try:
-        system = _SYSTEM_MAP[uname.system]
-        architecture = _ARCHITECTURE_MAP[uname.system][uname.machine]
+        system = _SYSTEM_MAP[platform.system()]
+        architecture = _ARCHITECTURE_MAP[get_cpu_info()["arch"]]
     except KeyError:
-        raise RuntimeError(
-            f"Unsupported platform: {uname.system} {uname.machine}"
-        ) from None
+        raise RuntimeError(f"Unsupported platform: {platform.uname()!r}") from None
     return system, architecture
 
 
 GOOS, GOARCH = _get_platform()
 
-ARCHIVE_EXT = ".zip" if GOOS == "windows" else ".tar.gz"
-EXECUTABLE_EXT = ".exe" if GOOS == "windows" else ""
+ARCHIVE_EXT = ".zip" if psutil.WINDOWS else ".tar.gz"
+EXECUTABLE_EXT = ".exe" if psutil.WINDOWS else ""
