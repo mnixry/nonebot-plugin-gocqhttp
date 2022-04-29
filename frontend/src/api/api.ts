02ca2f0562e8cb6ecd6caf7da744c3bfae3ffac7
Mix <mnixry@users.noreply.github.com>
2022-04-29 10:07:34 +0800

    :alien: Generate updated frontend API definition

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -114,7 +114,8 @@ export const AccountProtocol = {
     NUMBER_1: 1,
     NUMBER_2: 2,
     NUMBER_3: 3,
-    NUMBER_4: 4
+    NUMBER_4: 4,
+    NUMBER_5: 5
 } as const;
 
 export type AccountProtocol = typeof AccountProtocol[keyof typeof AccountProtocol];
