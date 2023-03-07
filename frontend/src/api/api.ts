782ec3f9277f664891e9459dac1260c66bfdd94c
Mix <32300164+mnixry@users.noreply.github.com>
2023-03-08 01:56:47 +0800

    :alien: Re-generate API binding code up-to-date

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -135,7 +135,8 @@ export const AccountProtocol = {
     NUMBER_2: 2,
     NUMBER_3: 3,
     NUMBER_4: 4,
-    NUMBER_5: 5
+    NUMBER_5: 5,
+    NUMBER_6: 6
 } as const;
 
 export type AccountProtocol = typeof AccountProtocol[keyof typeof AccountProtocol];
